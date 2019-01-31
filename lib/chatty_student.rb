df69d74471a393d74533fddf58a79586531a1a0d
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 20:02:07 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,7 +8,17 @@ class ChattyStudent < Student
   end     
   
   def raise_hand 
-    10.times do |
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+  end  
     
 
 end   
